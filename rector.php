f6f6d94a47e657c8f677e5fafddba21c08deba37
Frank van Hest <frank@frankvanhest.nl>
2023-03-21 19:56:39 +0100

    Added rector for PHP downgrade

---
+++ rector.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        DowngradeLevelSetList::DOWN_TO_PHP_81
+    ]);
+};
